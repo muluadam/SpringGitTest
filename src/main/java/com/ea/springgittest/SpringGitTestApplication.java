f037f267730d7191d0befc3faf18c65c2d7a7344
Muluadam Temesgen Akalu <mtmuluadam08@gmail.com>
2021-12-11 21:13:23 -0600

    First Commit

---
+++ src/main/java/com/ea/springgittest/SpringGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.ea.springgittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ea/springgittest/SpringGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.springgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
