f1963ec422b9380f552fd82b487df53577e6b28a
Natnael4 <87682063+Natnael4@users.noreply.github.com>
2021-12-11 21:40:07 -0600

    commit2

--- src/main/java/com/ea/springgittest/SpringGitTestApplication.java
+++ src/main/java/com/ea/springgittest/SpringGitTestApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringGitTestApplication {
 
+    public void func(){
+//        System.out.
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringGitTestApplication.class, args);
+
+
     }
 
 }
