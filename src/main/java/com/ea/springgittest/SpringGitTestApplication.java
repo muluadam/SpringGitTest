69df8a57afe1493a56008f603e2318950729c7bf
yohannes84 <34288731+yohannes84@users.noreply.github.com>
2021-12-11 21:37:23 -0600

    prinitng name: Yohannes
    yohanne
    src/main/java/com/ea/springgittest/SpringGitTestApplication.jav`

--- src/main/java/com/ea/springgittest/SpringGitTestApplication.java
+++ src/main/java/com/ea/springgittest/SpringGitTestApplication.java
@@ -8,6 +8,7 @@ public class SpringGitTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGitTestApplication.class, args);
-    }
 
+        System.out.println("This is Yohannes");
+    }
 }
